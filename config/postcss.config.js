18c41e41f7f3ef52cffe756ad05332929250b587
James Zaccardo <jameszaccardo@gmail.com>
2017-07-30 21:28:44 -0700

    added rucksack

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import'),
         require('lost'),
+        require('rucksack-css'),
         require('postcss-cssnext')({
             features: {
                 rem: false, // it will not add fallback from rem to px
